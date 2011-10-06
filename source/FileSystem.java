7b9fc8ed8b35884ed46fc9f0f723f31bc1f8964b
L0mion <davidgrelsson@gmail.com>
2011-10-06 12:03:39 +0200

    Small changes to FileSystem.java

--- source/FileSystem.java
+++ source/FileSystem.java
@@ -1,13 +1,22 @@
 /*This is the main class of the program*/
+
+
+//import java.util.ArrayList;
+
 public class FileSystem {
+	
+	/*Attributes*/
+	//private ArrayList<byte[]> blocks;
+	
 	/*Constructors*/
 	public FileSystem() {
+		
+	//	blocks = new ArrayList<byte[]>();
+		
 		System.out.println("Battlecruiser operational.");
 	}
 	
-	/*Members*/
-	
-	/*Attributes*/
-	
-	/*Methods*/
+	//   /*Members*/
+		
+	//	/*Methods*/
 }
