231183d7d664b1319081962bf5a4775730478995
Cater Pillar <CaterHatterPillar@gmail.com>
2011-10-06 12:00:47 +0200

    Added FileSystem.java

--- source/FileSystem.java
+++ source/FileSystem.java
@@ -1,13 +1,41 @@
+import java.util.*;
+
 /*This is the main class of the program*/
 public class FileSystem {
 	/*Constructors*/
 	public FileSystem() {
-		System.out.println("Battlecruiser operational.");
+		System.out.println("##Startup: Constructing FileSystem");
+		mCurrentDirectory = new Directory("root");
+		
+		mInput = new Scanner(System.in);
+		mRunning = true;
+		
+		start();
 	}
 	
 	/*Members*/
+	private boolean mRunning;
+	private Directory mCurrentDirectory;
+	private Scanner mInput;
 	
 	/*Attributes*/
 	
 	/*Methods*/
+	private void start() {
+		String currentInput;
+		String[] command;
+		while(mRunning) {
+			/*Get input*/
+			currentInput = mInput.nextLine();
+			
+			/*Interpet input*/
+			command = currentInput.split(" ", 100);
+			
+			for(int i = 0; i < command.length; i++) {
+				System.out.println(command[i]);
+			}
+			
+			/*Perform command*/
+		}
+	}
 }
