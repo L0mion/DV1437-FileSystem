223dcfb6ee39343ff75866c809cefe8e908e0a39
L0mion <davidgrelsson@gmail.com>
2011-10-06 11:39:41 +0200

    Fixed local repo after batshit merge conflict.

---
+++ source/File.java
@@ -0,0 +1,22 @@
+public class File extends Component {
+	
+	private int mDataPointer;
+	
+	public File() {
+			
+		super("default");
+		mDataPointer = -1;
+	}
+	
+	public File(int dataPointer, String name) {
+	
+		super(name);
+		
+		mDataPointer = dataPointer;
+	}
+	
+	public int getDataPointer() {
+	
+		return 
+	}
+}
