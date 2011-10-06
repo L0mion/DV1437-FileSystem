253318779ff4981c0e511e3a3d5585d36422e8d4
Cater Pillar <CaterHatterPillar@gmail.com>
2011-10-06 11:15:47 +0200

    Added FileSystem.java

---
+++ source/FileSystem.java
@@ -0,0 +1,13 @@
+/*This is the main class of the program*/
+public class FileSystem {
+	/*Constructors*/
+	public FileSystem() {
+		System.out.println("Battlecruiser operational.");
+	}
+	
+	/*Members*/
+	
+	/*Attributes*/
+	
+	/*Methods*/
+}
--- source/Main.java
+++ source/Main.java
@@ -1,6 +1,5 @@
 public class Main {
-
 	public static void main(String[] args) {
+		FileSystem mFileSystem; mFileSystem = new FileSystem();
 	}
-
 }
