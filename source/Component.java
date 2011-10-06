4286a112217f015a7533dcdcab819a969bfc87da
Cater Pillar <CaterHatterPillar@gmail.com>
2011-10-06 10:48:28 +0200

    Fixed component.

--- source/Component.java
+++ source/Component.java
@@ -1,14 +1,19 @@
 public abstract class Component {
 	/*Constructors*/
-	public Component() {
+	public Component(String name) {
+		mName = name;
+		mSize = -1;
 	}
 	
 	/*Attributes*/
 	private String mName;
 	private int mSize;
 	
-	public String getName() {
+	public String name() {
 		return mName;
+	}
+	public int size() {
+		return mSize;
 	}
 	
 	/*Methods*/
