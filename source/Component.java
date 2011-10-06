bfe5ff88da137b76c35985b11597a1cfb5e5f5a4
Cater Pillar <CaterHatterPillar@gmail.com>
2011-10-06 10:40:30 +0200

    Added Component-class

---
+++ source/Component.java
@@ -0,0 +1,15 @@
+public abstract class Component {
+	/*Constructors*/
+	public Component() {
+	}
+	
+	/*Attributes*/
+	private String mName;
+	private int mSize;
+	
+	public String getName() {
+		return mName;
+	}
+	
+	/*Methods*/
+}
