2dac2b7e200d37914603e73dd80245ce97f05dd2
L0mion <davidgrelsson@gmail.com>
2011-10-10 11:32:17 +0200

    New methods Directory.java Added methods to add and remove components in Directory.java

--- source/Directory.java
+++ source/Directory.java
@@ -5,13 +5,99 @@ public class Directory extends Component {
 	public Directory(String name) {
 		super(name);
 		
-		mFiles = new ArrayList<Component>();
+		mComponents = new ArrayList<Component>();
 	}
 	
 	/*Members*/
-	private ArrayList<Component> mFiles;
+	private ArrayList<Component> mComponents;
 	
 	/*Attributes*/
 	
 	/*Methods*/
+	public Component getComponent(String name) {
+	
+		/* 
+		 * Searches directory for component with specific name,
+		 * returns null if the component isn't found
+		 */ 
+		
+		Component returnComponent = null;
+		
+		for(int i=0; i<mComponents.size(); i++)
+		{
+			if(mComponents.get(i).name().equals(name)) {
+				
+				returnComponent = mComponents.get(i);
+			}
+		}
+		
+		return returnComponent;
+	}
+	
+	public Component getComponent(int index) {
+	
+		/* 
+		 * Returns component that resides on the given index.
+		 * Returns null if the index is invalid
+		 */ 
+		
+		Component returnComponent = null;
+	
+		if(index>=0 && index<mComponents.size()) {
+		
+			returnComponent = mComponents.get(index);
+		}
+		
+		return returnComponent;
+	}
+	
+	public void addComponent(Component component) {
+	
+		mComponents.add(component);
+	}
+	
+	public boolean removeComponent(Component component) {
+	
+		boolean success = false;
+		success = mComponents.remove(component);
+		
+		return success;
+	} 
+	
+	public boolean removeComponent(int index) {
+	
+		boolean success = false;
+		Component temp = null;
+	
+		if(index>=0 && index<mComponents.size()) {
+		
+			temp = mComponents.remove(index);
+			
+			if(temp != null) {
+				success = true;	
+			}
+		}
+		
+		return success;
+	}
+	
+	public boolean removeComponent(String name) {
+	
+		boolean success = false;
+		Component temp = null;
+		
+		for(int i=0; i<mComponents.size(); i++) {
+		
+			if(mComponents.get(i).name().equals(name)) {
+			
+				temp = mComponents.remove(i);
+				
+				if(temp != null) {
+					success = true;
+				}
+			}
+		}
+		
+		return success;
+	}
 }
